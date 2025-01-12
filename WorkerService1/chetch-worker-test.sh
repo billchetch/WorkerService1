13045d7b0af2843b5343f8d03e1ae7463e324211
bill@chetch.net <bill@chetch.net>
2025-01-12 18:00:01 +0800

    updated service and .sh file

---
+++ WorkerService1/chetch-worker-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Run the service as a FDD
+/home/pi/.dotnet/dotnet /home/pi/Documents/VisualStudioProjects/Tests/WorkerService1/WorkerService1/bin/Debug/net9.0/WorkerService1.dll
+#/home/pi/.dotnet/dotnet --version
+#echo "Hi there"

52dcb1cde12ecd65e7df713e55c1294e9da6a366
billchetch <45006134+billchetch@users.noreply.github.com>
2025-01-12 14:24:01 +0700

    xxx

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -4,6 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        //create builder and do some things
         var builder = Host.CreateApplicationBuilder(args);
         builder.Services.AddSystemd();
         builder.Services.AddHostedService<Worker>();
